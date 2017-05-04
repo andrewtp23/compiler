62775302c6ad954bbf8d839445eaf727162f0a8e
andrewtp23 <atp23@zips.uakron.edu>
2017-05-03 22:42:54 -0400

    changed stuff

--- ast.hpp
+++ ast.hpp
@@ -313,4 +313,27 @@ struct var_decl : Decl {
 	var_decl(symbol* n, Type* t) : name(n), ty(t) { }
 };
 
+struct state {
+	virtual ~state() { };
+};
+
+struct expr_state : state {
+	expr_state(Expr* e) : e(e) { }
+	Expr* e;
+};
+
+struct decl_state : state {
+	decl_state(Decl* d) : entity(d) { }
+	Decl* entity;
+};
+
+struct if_state : state {
+	if_state(Expr* e, state* s1, state* s2) : condition(e), s1(s1), s2(s2) { }
+	
+	Expr* condition;
+	state* s1;
+	state* s2;
+	
+};
+
 #endif
